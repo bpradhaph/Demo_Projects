dca1822d0075d176c66b90686825d3e45e0cafb1
pradhaph276 <70710404+pradhaph276@users.noreply.github.com>
2026-05-10 06:02:14 +0530

    login screen add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
 
 
-      <h1>App Component</h1>
+      <Login />
     
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+
+      <input type="text" placeholder="Username" />
+      <br /><br />
+
+      <input type="password" placeholder="Password" />
+      <br /><br />
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
